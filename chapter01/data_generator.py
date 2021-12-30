f47f6ac161aa11091fb9b4db876fac30519c3de7
joshualat <akosijoshualat@gmail.com>
2021-12-30 23:25:46 +0000

    Update data_generator.py

--- chapter01/data_generator.py
+++ chapter01/data_generator.py
@@ -6,20 +6,20 @@ from utils import block, debug
 with block('LOAD CTGAN'):
     pkl = './hotel_bookings.gan.pkl'
     gan = CTGANSynthesizer.load(pkl)
-    print(gan)
+    print(gan.__dict__)
 
 
-with block('GENERATE SYNTHETIC DATA'):
-    synthetic_data = gan.sample(10000)
-    print(synthetic_data)
+# with block('GENERATE SYNTHETIC DATA'):
+#     synthetic_data = gan.sample(10000)
+#     print(synthetic_data)
 
 
-with block('SAVE TO CSV'):
-    target_location = "tmp/bookings.all.csv"
-    synthetic_data.to_csv(target_location, index=False)
+# with block('SAVE TO CSV'):
+#     target_location = "tmp/bookings.all.csv"
+#     synthetic_data.to_csv(target_location, index=False)
 
 
-with block('GENERATE PROFILE REPORT'):
-    profile = ProfileReport(synthetic_data)
-    target_location = "tmp/profile-report.html"
-    profile.to_file(target_location)
+# with block('GENERATE PROFILE REPORT'):
+#     profile = ProfileReport(synthetic_data)
+#     target_location = "tmp/profile-report.html"
+#     profile.to_file(target_location)
