6777846e98da3730247a1c2eaa6c3e1ca6008f0d
Joshua Arvin Lat <akosijoshualat@gmail.com>
2022-08-14 05:24:54 +0800

    Create deploy-model-to-new-endpoint.py

---
+++ chapter11/deploy-model-to-new-endpoint.py
@@ -0,0 +1,40 @@
+import json
+
+from utils import (
+    create_model, 
+    create_endpoint_config, 
+    create_endpoint, 
+    random_string,
+    block
+)
+
+def lambda_handler(event, context):
+    role = event['role']
+    endpoint_name = event['endpoint_name']
+    package_arn = event['package_arn']
+    
+    model_name = 'model-' + random_string()
+    
+    with block('CREATE MODEL'):
+        create_model(
+            model_name=model_name,
+            package_arn=package_arn,
+            role=role
+        )
+    
+    with block('CREATE ENDPOINT CONFIG'):
+        endpoint_config_name = create_endpoint_config(
+            model_name
+        )
+    
+    with block('CREATE ENDPOINT'):
+        create_endpoint(
+            endpoint_name=endpoint_name, 
+            endpoint_config_name=endpoint_config_name
+        )
+        
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event),
+        'model': model_name
+    }
