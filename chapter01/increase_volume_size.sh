85a63e5691d648755cfaa65d6c140937f24c2024
Joshua Arvin Lat <akosijoshualat@gmail.com>
2022-09-05 17:57:17 +0800

    Delete increase_volume_size.sh

--- chapter01/increase_volume_size.sh
+++
@@ -1,6 +0,0 @@
-pip3 install --user --upgrade boto3
-
-TARGET_METADATA_URL=http://169.254.169.254/latest/meta-data/instance-id
-export EC2_INSTANCE_ID=$(curl -s $TARGET_METADATA_URL)
-
-python3 resize_and_reboot.py
